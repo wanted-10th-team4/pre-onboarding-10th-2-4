fd6c7a4542eb58c22d7ea15445588466642a436a
gomsang <tkdgh3050@gmail.com>
2023-05-04 17:53:30 +0900

    chore: netlify proxy 설정 #19

--- src/api/search.ts
+++ src/api/search.ts
@@ -3,12 +3,14 @@ import { getCacheByKey, setCacheByExpireTime } from '@utils/cache';
 import { CACHE_EXPIRE_TIME } from '@utils/constant';
 import axios from 'axios';
 
+const PROXY = window.location.hostname === 'localhost' ? '' : '/proxy';
+
 const getSearchResults = async (param: SearchParam): Promise<GetSearchResultsResponse> => {
   const cacheItem = getCacheByKey(param.name);
   if (cacheItem) return cacheItem;
 
   console.info('calling api');
-  const response = await axios.get(`${process.env.REACT_APP_API_URI}`, { params: param });
+  const response = await axios.get(`${PROXY}${process.env.REACT_APP_API_URI}`, { params: param });
 
   setCacheByExpireTime({ key: param.name, value: response.data, expireTime: CACHE_EXPIRE_TIME });
   return response.data;
